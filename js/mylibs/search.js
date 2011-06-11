10852e29ecf0b57bbac0ece17c4845dbc2a569d5
Harley <harley@bigdoor.com>
2011-06-11 13:14:02 -0700

    Basic cocktail search implemented

---
+++ js/mylibs/search.js
@@ -0,0 +1,38 @@
+(function(CS) {
+
+	function can_make(ingrediants, drink, max_missing) {
+		var drink_ingrediants = drink.ingrediants,
+			missing = 0,
+			have = 0;
+		for ( var i = 0; i < drink_ingrediants.length; i++ ) {
+			if ( !(drink_ingrediants[i].name.toLowerCase() in ingrediants) ) {
+				missing = missing + 1;
+				if ( missing > max_missing ) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	function search(ingrediants, drinks, max_missing) {
+		if ( max_missing === undefined ) {
+			max_missing = 0;
+		}
+
+		var results = [];
+
+		for (var i = 0; i < drinks.length; i++) {
+			if ( can_make(ingrediants, drinks[i], max_missing) ) {
+				results.push(drinks[i]);
+			}
+		};
+
+		return results;
+
+	}
+
+	CS.can_make = can_make;
+	CS.search = search;
+
+})(CS)
--- test/tests.js
+++ test/tests.js
@@ -22,5 +22,72 @@ test('Environment is good',function(){
   ok( !!window.Modernizr, 'Modernizr global is present')
 })
 
+module("search tests");
+var search = CS.search,
+	can_make = CS.can_make;
+test('search of no drinks returns an empty array', function() {
+	var results = search([], [], 0);
+	ok(results.length === 0);
+});
 
+var ingrediants = {
+	'bourbon': true, 
+	'bitters': true,
+	'orange': true,
+	'cherry': true,
+	'sugar': true
+}
 
+var old_fashioned = {
+	"name": "Old Fashioned",
+	"ingrediants": [
+		{"name": "Bourbon"},
+		{"name": "Bitters"},
+		{"name": "Orange"},
+		{"name": "Cherry"},
+		{"name": "Sugar"}
+	],
+	"container": {"name": "Old Fashioned"}
+}
+
+var martini = {
+	"name": "Martini",
+	"ingrediants": [
+		{"name": "Vodka"},
+		{"name": "Olives"},
+		{"name": "Vermouth"}
+	],
+	"container": {"name": "Martini"}
+}
+
+test('search of drinks with no ingrediants returns all drinks', function() {
+	var empty_glass = {
+		"name": "Disappointment",
+		"ingrediants": [],
+		"container": "Pint"
+	}
+	var results = search(ingrediants, [empty_glass], 0);
+	ok(results.length === 1);
+});
+
+test('can make old_fashioned', function() {
+	var results = can_make(ingrediants, old_fashioned, 0);
+	ok(results);
+});
+
+test('can not make martini', function() {
+	var results = can_make(ingrediants, martini, 0);
+	ok(!results);
+});
+
+test('search of only one drink which has exact ingrediants returns that drink', function() {
+	var results = search(ingrediants, [old_fashioned], 0);
+	ok(results.length === 1);
+});
+
+test('search of two drinks one of which mathches returns a single drink',
+function() {
+	var results = search(ingrediants, [old_fashioned, martini], 0);
+	console.log(results);
+	ok(results.length === 1);
+});
