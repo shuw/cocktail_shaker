61386ee31fa770b65ac5991577ffccaaad43829e
Shu Wu <shuwu83@gmail.com>
2011-06-12 15:16:43 -0700

    chart width fix

--- js/mylibs/swizzlePanel.js
+++ js/mylibs/swizzlePanel.js
@@ -155,16 +155,17 @@ function getVisualDisplay(ingredients) {
 	var chartContainer = $("#result-chart");
 	var chart = new google.visualization.PieChart(chartContainer[0]);
 	chart.draw(data, {
-		width: 280,
-		height: 150,
+		width: 300,
+		height: 140,
 		is3D: true,
 		chartArea: {
 			left: 0,
 			right: 0,
-			height: 140,
-			width: 180
+			height: 120,
+			width: 290
 		},
 		backgroundColor: "#E6E6E6",
+		legendTextStyle: "width: 300px;",
 		legend: 'right'
 	});
 }
